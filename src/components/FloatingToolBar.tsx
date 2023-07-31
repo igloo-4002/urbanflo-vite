e9be7914f1bac352b7de959fc0feea4135a51452
Taz17 <mutaz.ashhab@gmail.com>
2023-07-31 15:16:30 +1000

    add `FloatingToolBar`

---
+++ src/components/FloatingToolBar.tsx
@@ -0,0 +1,20 @@
+export default function FloatingToolBar() {
+    return (
+      <span
+        style={{
+          position: "fixed",
+          top: 15,
+          left: "50%",
+          transform: "translateX(-50%)",
+          zIndex: 1000,
+          backgroundColor: "#FAF9F6",
+          padding: "8px 18px",
+          borderRadius: "10px",
+          boxShadow: "rgba(99, 99, 99, 0.2) 0px 2px 8px 0px",
+        }}
+      >
+        Tool bar items component - not yet implemented
+      </span>
+    );
+  }
+  
