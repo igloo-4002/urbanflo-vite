2961c8cecf137a446f6d249a80c16e06112d59ed
Taz17 <mutaz.ashhab@gmail.com>
2023-08-07 19:09:46 +1000

    update `selectedToolBarItem` when toolbar item is selected and update styling

--- src/components/ToolBar/ToolBarItem.tsx
+++ src/components/ToolBar/ToolBarItem.tsx
@@ -1,20 +1,45 @@
+import classNames from 'classnames';
+
+import { ToolBarItemOptions } from '~/context/types';
+import { useAppState } from '~/hooks/useAppState';
 
 export interface ToolBarItemProps {
-    src: string;
-    alt: string;
-    onClick: () => void;
+  src: string;
+  alt: string;
+  optionName: ToolBarItemOptions;
+  onClick: () => void;
 }
 
 export function ToolBarItem(props: ToolBarItemProps) {
+  const { appState, setAppState } = useAppState();
+
+  function handleClick() {
+    props.onClick();
+    setAppState(prev => {
+      return {
+        ...prev,
+        toolBarState: {
+          ...prev.toolBarState,
+          selectedToolBarItem: props.optionName,
+        },
+      };
+    });
+  }
+
+  const cls = classNames({
+    'hover:bg-gray-300 p-1 rounded-md duration-200': true,
+    'bg-gray-300':
+      props.optionName === appState.toolBarState.selectedToolBarItem,
+  });
 
   return (
-    <button onClick={props.onClick} className='hover:bg-gray-300 p-1 rounded-md duration-200'>
+    <button onClick={handleClick} className={cls}>
       <img
         src={props.src}
         alt={props.alt}
         style={{
-            height: "24px",
-            width: "24px",
+          height: '24px',
+          width: '24px',
         }}
       />
     </button>
