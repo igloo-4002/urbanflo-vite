38b4a933ba04b05c52fc90d0706da8190a66d9f1
Taz17 <mutaz.ashhab@gmail.com>
2023-08-08 23:34:39 +1000

    use `NativeDivProps` for Stack components

--- src/components/Stacks.tsx
+++ src/components/Stacks.tsx
@@ -1,22 +1,15 @@
-import { type CSSProperties, type ReactNode } from 'react';
+import { AllHTMLAttributes } from 'react';
 
-interface StackProps {
-  style?: CSSProperties;
-  children: ReactNode;
+type NativeDivProps = AllHTMLAttributes<HTMLDivElement>;
+
+export function ColumnStack(props: NativeDivProps) {
+  return <div className="flex flex-col">{props.children}</div>;
 }
 
-export function ColumnStack({ style = {}, children }: StackProps) {
+export function RowStack(props: NativeDivProps) {
   return (
-    <div style={style} className="flex flex-col">
-      {children}
-    </div>
-  );
-}
-
-export function RowStack({ style = {}, children }: StackProps) {
-  return (
-    <div style={style} className="flex flex-row items-center justify-between">
-      {children}
+    <div className="flex flex-row items-center justify-between">
+      {props.children}
     </div>
   );
 }
