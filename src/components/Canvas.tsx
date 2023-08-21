ee39b999e68c5cdc1dd31933e6dc6c5577b69c57
Taz17 <mutaz.ashhab@gmail.com>
2023-08-21 15:43:22 +1000

    select instead of deselect

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -159,7 +159,7 @@ export function Canvas() {
                     );
                     selector.deselect();
                   } else {
-                    selector.deselect();
+                    selector.select(node.id);
                   }
                 }}
               />
