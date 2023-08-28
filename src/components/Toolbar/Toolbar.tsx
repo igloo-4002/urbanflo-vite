4c49a74cac227108ba69f5d284eb3ddd9cec350a
Taz17 <mutaz.ashhab@gmail.com>
2023-08-28 17:20:43 +1000

    Add `Toolbar` and `ToolbarItem` components

---
+++ src/components/Toolbar/Toolbar.tsx
@@ -0,0 +1,28 @@
+import { useToolbarState } from '~/zustand/useToolbar';
+
+import { ToolBarItem } from './ToolbarItem';
+
+export function FloatingToolBar() {
+  const toolbarState = useToolbarState();
+
+  return (
+    <span
+      style={{
+        position: 'fixed',
+        display: toolbarState.isOpen ? 'flex' : 'none',
+        top: 115,
+        left: '50%',
+        transform: 'translateX(-50%)',
+        zIndex: 1000,
+        backgroundColor: '#FAF9F6',
+        padding: '8px',
+        borderRadius: '10px',
+        boxShadow: 'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+      }}
+    >
+      {toolbarState.items.map((item, idx) => {
+        return <ToolBarItem toolbarItem={item} key={idx} />;
+      })}
+    </span>
+  );
+}
