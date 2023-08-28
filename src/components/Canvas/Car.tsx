c4bdec72823b4918483c0051863b70cb1247ddbb
Taz17 <mutaz.ashhab@gmail.com>
2023-08-28 15:05:40 +1000

    revert to old car component

--- src/components/Canvas/Car.tsx
+++ src/components/Canvas/Car.tsx
@@ -1,4 +1,4 @@
-import { Circle, Group, Rect } from 'react-konva';
+import { Circle } from 'react-konva';
 
 import { Car as CarType } from '~/zustand/useCarStore';
 
@@ -7,39 +7,13 @@ interface CarProps {
 }
 
 export function Car({ car }: CarProps) {
-  const carWidth = 20;
-  const carLength = 40;
-  const wheelSize = 4;
-
-  const { x, y } = car.location;
-
-  // Calculate the positions for the car body and wheels
-  const bodyX = x - carWidth / 2;
-  const bodyY = y - carLength / 2;
-
   return (
-    <Group>
-      {/* Car Body */}
-      <Rect
-        x={bodyX}
-        y={bodyY}
-        width={carWidth}
-        height={carLength}
-        fill={car.color}
-      />
-      {/* Front-left Wheel */}
-      <Circle x={bodyX} y={bodyY} radius={wheelSize} fill="black" />
-      {/* Front-right Wheel */}
-      <Circle x={bodyX + carWidth} y={bodyY} radius={wheelSize} fill="black" />
-      {/* Rear-left Wheel */}
-      <Circle x={bodyX} y={bodyY + carLength} radius={wheelSize} fill="black" />
-      {/* Rear-right Wheel */}
-      <Circle
-        x={bodyX + carWidth}
-        y={bodyY + carLength}
-        radius={wheelSize}
-        fill="black"
-      />
-    </Group>
+    <Circle
+      width={5}
+      height={5}
+      x={car.location.x}
+      y={car.location.y}
+      fill={car.color}
+    />
   );
 }
