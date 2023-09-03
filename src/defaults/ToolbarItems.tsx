f3acfac1f399bbba0e91c8e5c78f09523d677c31
Taz17 <mutaz.ashhab@gmail.com>
2023-09-03 20:32:55 +1000

    use `LabelNames`

--- src/defaults/ToolbarItems.tsx
+++ src/defaults/ToolbarItems.tsx
@@ -1,13 +1,13 @@
 import { CursorArrowRaysIcon } from '@heroicons/react/24/outline';
 
-import { ToolbarItem } from '~/types/Toolbar';
+import { LabelNames, ToolbarItem } from '~/types/Toolbar';
 
 import intersectionIcon from '../../public/intersection.png';
 import roadIcon from '../../public/road-icon.png';
 
 export const toolbarItems: ToolbarItem[] = [
   {
-    label: 'Pointer',
+    label: LabelNames.Pointer,
     heroIcon: <CursorArrowRaysIcon className="h-6 w-6" />,
     onClick: () => {
       console.log('Pointer icon clicked');
@@ -17,14 +17,14 @@ export const toolbarItems: ToolbarItem[] = [
     divider: true,
   },
   {
-    label: 'Intersection',
+    label: LabelNames.Intersection,
     icon: intersectionIcon,
     onClick: () => {
       console.log('Intersection icon clicked');
     },
   },
   {
-    label: 'Road',
+    label: LabelNames.Road,
     icon: roadIcon,
     onClick: () => {
       console.log('Road icon clicked');
