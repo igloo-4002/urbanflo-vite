9699eb44fdb8b26ea5c6fa1ad912a0a70a491d47
Taz17 <mutaz.ashhab@gmail.com>
2023-09-11 13:07:43 +1000

    add `BidirectionalRoad`

--- src/components/Canvas/BidirectionalRoad.tsx
+++ src/components/Canvas/BidirectionalRoad.tsx
@@ -1,17 +1,40 @@
+import { Arrow } from 'react-konva';
+
 import { Edge } from '~/types/Network';
+import { useNetworkStore } from '~/zustand/useNetworkStore';
+
+import { Road, laneWidth } from './Road';
 
 interface BidirectionalRoadProps {
   edge: Edge;
   oppositeEdge: Edge;
 }
 
-export const laneWidth = 25;
-
 export function BidirectionalRoad({
   edge,
   oppositeEdge,
 }: BidirectionalRoadProps) {
-  console.log(edge, oppositeEdge);
+  const networkStore = useNetworkStore();
 
-  return <></>;
+  const road1TotalWidth = laneWidth * edge.numLanes;
+  const road2TotalWidth = laneWidth * oppositeEdge.numLanes;
+
+  const from = networkStore.nodes[edge.from];
+  const to = networkStore.nodes[edge.to];
+
+  return (
+    <>
+      <Road edge={edge} offset={-road1TotalWidth / 2} />
+      <Road edge={oppositeEdge} offset={-road2TotalWidth / 2} />
+      <Arrow
+        x={from.x}
+        y={from.y}
+        points={[0, 0, to.x - from.x, to.y - from.y]}
+        stroke="white"
+        strokeWidth={2}
+        pointerWidth={0}
+        pointerLength={0}
+      />
+    </>
+  );
 }
