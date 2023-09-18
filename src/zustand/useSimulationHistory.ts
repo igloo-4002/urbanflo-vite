3a625b9bc2a5244537d5e2b184b46b7c5f734f9e
Taz17 <mutaz.ashhab@gmail.com>
2023-09-18 12:53:57 +1000

    add `showHistory` and methods to manipulate it

--- src/zustand/useSimulationHistory.ts
+++ src/zustand/useSimulationHistory.ts
@@ -15,11 +15,17 @@ export interface SimulationHistory {
 
 type SimulationHistoryState = {
   history: SimulationHistory[];
+  showHistory: boolean;
+  closeHistory: () => void;
+  openHistory: () => void;
   updateHistory: (s: SimulationHistory) => void;
 };
 
 export const useSimulationHistory = create<SimulationHistoryState>(set => ({
   history: [],
+  showHistory: false,
+  closeHistory: () => set({ showHistory: false }),
+  openHistory: () => set({ showHistory: true }),
   updateHistory: (s: SimulationHistory) =>
     set(state => ({
       history: [...state.history, s],
