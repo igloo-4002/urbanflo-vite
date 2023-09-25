7d55c4cbc92e560cbfbbc1a441d7415be3aeaf85
Taz17 <mutaz.ashhab@gmail.com>
2023-09-25 19:45:12 +1000

    add `RemoveEdgeCommand`

---
+++ src/helpers/commands/RemoveEdgeCommand.ts
@@ -0,0 +1,27 @@
+import { Edge } from '~/types/Network';
+import { Network } from '~/zustand/useNetworkStore';
+import { Command } from '~/zustand/useUndoStore';
+
+export class RemoveEdgeCommand extends Command {
+  constructor(
+    private network: Network,
+    private edge: Edge,
+  ) {
+    super();
+  }
+
+  execute() {
+    this.network.deleteEdge(this.edge.id);
+  }
+
+  unexecute() {
+    const fromNode = this.network.nodes[this.edge.from];
+    const toNode = this.network.nodes[this.edge.to];
+
+    if (!fromNode && !toNode) {
+      return;
+    }
+
+    this.network.drawEdge(fromNode, toNode);
+  }
+}
