504fc3d8d60dd5a691d91d51bcbd3ae83dea0039
Taz17 <mutaz.ashhab@gmail.com>
2023-09-26 12:30:24 +1000

    Add inital `ToolTip` component

---
+++ src/components/Tooltip.tsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from 'react';
+
+interface TooltipProps {
+  tooltipText: string;
+  children: React.ReactNode;
+}
+
+export function Tooltip({ tooltipText, children }: TooltipProps) {
+  const [visible, setVisible] = useState(false);
+  const tooltipRef = useRef<HTMLDivElement | null>(null);
+
+  const showTooltip = () => setVisible(true);
+  const hideTooltip = () => setVisible(false);
+
+  return (
+    <div className="relative inline-block">
+      <div
+        onMouseEnter={showTooltip}
+        onMouseLeave={hideTooltip}
+        onFocus={showTooltip}
+        onBlur={hideTooltip}
+      >
+        {children}
+      </div>
+      <div
+        ref={tooltipRef}
+        className={`absolute z-10 ${
+          visible ? 'opacity-100' : 'opacity-0 invisible'
+        } inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm dark:bg-gray-700 transition-opacity duration-300`}
+        role="tooltip"
+      >
+        {tooltipText}
+        <div className="tooltip-arrow" data-popper-arrow></div>
+      </div>
+    </div>
+  );
+}
+
+export default Tooltip;
