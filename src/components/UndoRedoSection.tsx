2e5ee860ae81734fcfa3ad34dc849152c2549dc4
Taz17 <mutaz.ashhab@gmail.com>
2023-09-26 12:33:02 +1000

    add tooltips to undo redo icons

--- src/components/UndoRedoSection.tsx
+++ src/components/UndoRedoSection.tsx
@@ -5,17 +5,23 @@ import {
 
 import { useUndoStore } from '~/zustand/useUndoStore';
 
+import Tooltip from './Tooltip';
+
 export function UndoRedoSection() {
   const undoStore = useUndoStore();
 
   return (
     <div className="flex flex-row items-center gap-3">
-      <button onClick={undoStore.undo} className="bg-gray-200 p-1 rounded-xl">
-        <ArrowUturnLeftIcon className="h-6 w-6 text-gray-800" />
-      </button>
-      <button onClick={undoStore.redo} className="bg-gray-200 p-1 rounded-xl">
-        <ArrowUturnRightIcon className="h-6 w-6 text-gray-800" />
-      </button>
+      <Tooltip tooltipText="Undo">
+        <button onClick={undoStore.undo} className="bg-gray-200 p-1 rounded-xl">
+          <ArrowUturnLeftIcon className="h-6 w-6 text-gray-800" />
+        </button>
+      </Tooltip>
+      <Tooltip tooltipText="Redo">
+        <button onClick={undoStore.redo} className="bg-gray-200 p-1 rounded-xl">
+          <ArrowUturnRightIcon className="h-6 w-6 text-gray-800" />
+        </button>
+      </Tooltip>
     </div>
   );
 }
