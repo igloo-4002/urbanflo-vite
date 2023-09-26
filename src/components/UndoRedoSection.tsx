0063766c3d64f682fe8ff43b528601e54598000a
Taz17 <mutaz.ashhab@gmail.com>
2023-09-26 12:21:31 +1000

    call `undo`/`redo` on click

--- src/components/UndoRedoSection.tsx
+++ src/components/UndoRedoSection.tsx
@@ -3,23 +3,17 @@ import {
   ArrowUturnRightIcon,
 } from '@heroicons/react/24/outline';
 
+import { useUndoStore } from '~/zustand/useUndoStore';
+
 export function UndoRedoSection() {
+  const undoStore = useUndoStore();
+
   return (
     <div className="flex flex-row items-center gap-3">
-      <button
-        onClick={() => {
-          console.log('out');
-        }}
-        className="bg-gray-200 p-1 rounded-xl"
-      >
+      <button onClick={undoStore.undo} className="bg-gray-200 p-1 rounded-xl">
         <ArrowUturnLeftIcon className="h-6 w-6 text-gray-800" />
       </button>
-      <button
-        onClick={() => {
-          console.log('in');
-        }}
-        className="bg-gray-200 p-1 rounded-xl"
-      >
+      <button onClick={undoStore.redo} className="bg-gray-200 p-1 rounded-xl">
         <ArrowUturnRightIcon className="h-6 w-6 text-gray-800" />
       </button>
     </div>
