4616614388a4f420c23ea5ab9a393b5566287b18
Taz17 <mutaz.ashhab@gmail.com>
2023-09-26 11:00:50 +1000

    use `ts-jest` for ts files

--- jest.config.mjs
+++ jest.config.mjs
@@ -167,7 +167,9 @@ const config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
