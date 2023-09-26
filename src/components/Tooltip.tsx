4da26942d4b0a9488ab34e229fc1b71fa43f3372
Taz17 <mutaz.ashhab@gmail.com>
2023-09-26 12:38:17 +1000

    refactor prop name & add `whitespace-nowrap`

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -1,11 +1,11 @@
 import { useState } from 'react';
 
 interface TooltipProps {
-  tooltipText: string;
+  text: string;
   children: React.ReactNode;
 }
 
-export function Tooltip({ tooltipText, children }: TooltipProps) {
+export function Tooltip({ text, children }: TooltipProps) {
   const [visible, setVisible] = useState(false);
 
   const showTooltip = () => setVisible(true);
@@ -24,10 +24,10 @@ export function Tooltip({ tooltipText, children }: TooltipProps) {
       <div
         className={`absolute z-10 bottom-full left-1/2 transform -translate-x-1/2 ${
           visible ? 'opacity-100' : 'opacity-0 invisible'
-        } inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm dark:bg-gray-700 transition-opacity duration-300`}
+        } inline-block px-3 py-2 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm dark:bg-gray-700 transition-opacity duration-300 whitespace-nowrap`}
         role="tooltip"
       >
-        {tooltipText}
+        {text}
         <div className="tooltip-arrow" data-popper-arrow></div>
       </div>
     </div>
