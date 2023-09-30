0e15fabc04ccca9d13dda4c5cbb270a8a2c0d641
Taz17 <mutaz.ashhab@gmail.com>
2023-09-30 23:19:55 +1000

    remove unused imports

--- src/defaults/ToolbarItems.tsx
+++ src/defaults/ToolbarItems.tsx
@@ -1,10 +1,8 @@
 import { CursorArrowRaysIcon } from '@heroicons/react/24/outline';
-import { LinkIcon } from '@heroicons/react/24/solid';
 
 import intersectionIcon from '~/assets/intersection.png';
 import roadIcon from '~/assets/road-icon.png';
 import { LabelNames, ToolbarItem } from '~/types/Toolbar';
-import { useToolbarStore } from '~/zustand/useToolbar';
 
 export const toolbarItems: ToolbarItem[] = [
   {
