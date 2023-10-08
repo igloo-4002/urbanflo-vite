96687dca4d3761cfb0592319740ddaa5f0dc048e
elysiacasandra <egia0002@student.monash.edu>
2023-10-09 09:32:59 +1100

    Remove redundant side bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -39,16 +39,6 @@ export function Header() {
             onChange={e => network.setDocumentName(e.target.value)}
           />
         </div>
-        <div className="flex lg:hidden">
-          <button
-            type="button"
-            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-            onClick={() => console.log('this does nothing')}
-          >
-            <span className="sr-only">Open main menu</span>
-            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
-          </button>
-        </div>
 
         <div className="hidden lg:flex lg:flex-1 lg:justify-end space-x-4">
           <ProjectDownloadButton />
