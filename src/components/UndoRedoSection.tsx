fb7f7e2a38529f1f069729b970a04f8f7116065c
Taz17 <mutaz.ashhab@gmail.com>
2023-10-16 09:33:43 +1100

    disable undo/redo when stack is empty

--- src/components/UndoRedoSection.tsx
+++ src/components/UndoRedoSection.tsx
@@ -13,12 +13,28 @@ export function UndoRedoSection() {
   return (
     <div className="flex flex-row items-center gap-3">
       <Tooltip text="Undo">
-        <button onClick={undoStore.undo} className="bg-gray-200 p-1 rounded-xl">
+        <button
+          onClick={undoStore.undo}
+          className={`bg-gray-200 p-1 rounded-xl ${
+            undoStore.undoStack.length === 0
+              ? 'cursor-not-allowed opacity-50'
+              : ''
+          }`}
+          disabled={undoStore.undoStack.length === 0}
+        >
           <ArrowUturnLeftIcon className="h-6 w-6 text-gray-800" />
         </button>
       </Tooltip>
       <Tooltip text="Redo">
-        <button onClick={undoStore.redo} className="bg-gray-200 p-1 rounded-xl">
+        <button
+          onClick={undoStore.redo}
+          className={`bg-gray-200 p-1 rounded-xl ${
+            undoStore.redoStack.length === 0
+              ? 'cursor-not-allowed opacity-50'
+              : ''
+          }`}
+          disabled={undoStore.redoStack.length === 0}
+        >
           <ArrowUturnRightIcon className="h-6 w-6 text-gray-800" />
         </button>
       </Tooltip>
