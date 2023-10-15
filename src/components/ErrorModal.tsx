3ea801624e51cf1498b0137e38805be10871e1a2
Akbar Fadiansyah <akbarfadi01@gmail.com>
2023-10-15 20:01:04 +1100

    Minor tweaks

--- src/components/ErrorModal.tsx
+++ src/components/ErrorModal.tsx
@@ -3,7 +3,7 @@ import { Fragment, useRef } from 'react';
 
 
 import { Dialog, Transition } from '@headlessui/react';
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline';
+import { XCircleIcon } from '@heroicons/react/24/outline';
 
 
 
@@ -52,7 +52,7 @@ export default function ErrorModal() {
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
                     <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                      <ExclamationTriangleIcon
+                      <XCircleIcon
                         className="h-6 w-6 text-red-600"
                         aria-hidden="true"
                       />
@@ -62,13 +62,11 @@ export default function ErrorModal() {
                         as="h3"
                         className="text-base font-semibold leading-6 text-gray-900"
                       >
-                        Deactivate account
+                        {errorModal.heading}
                       </Dialog.Title>
                       <div className="mt-2">
                         <p className="text-sm text-gray-500">
-                          Are you sure you want to deactivate your account? All
-                          of your data will be permanently removed. This action
-                          cannot be undone.
+                          {errorModal.body}
                         </p>
                       </div>
                     </div>
--- src/zustand/useErrorModal.ts
+++ src/zustand/useErrorModal.ts
@@ -3,16 +3,16 @@ import { create } from 'zustand';
 type ErrorModal = {
   isOpen: boolean;
   heading: string;
-  content: string;
+  body: string;
   open: (heading: string, content: string) => void;
   close: () => void;
 };
 
 export const useErrorModal = create<ErrorModal>(set => ({
-  isOpen: true,
+  isOpen: false,
   heading: '',
-  content: '',
-  open: (heading: string, content: string) =>
-    set({ isOpen: true, heading, content }),
+  body: '',
+  open: (heading: string, body: string) =>
+    set({ isOpen: true, heading, body }),
   close: () => set({ isOpen: false }),
 }));
