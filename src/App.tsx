eeba6c14594f87fa1cecdcc3128433a931e3361b
Vishal Jakhar <vishaljakhar60@yahoo.com>
2023-10-15 19:55:51 +1100

    block canvas on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,33 @@ import { LeftSideBar } from './components/SideBar';
 import { SimulationTimer } from './components/SimulationTimer';
 import { Toolbar } from './components/Toolbar/Toolbar';
 
+function MobileBlocker() {
+  return (
+    <div className="fixed inset-0 bg-white flex-col items-center justify-center p-4 flex">
+      <h1 className="text-2xl font-bold mb-4">Sorry for the inconvenience!</h1>
+      <p className="text-center mb-8">
+        Unfortunately, our app is not available on mobile devices yet. Please
+        visit us from your desktop for the full experience.
+      </p>
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <div className="h-screen w-screen">
-      <Header />
-      <Canvas />
-      <LeftSideBar />
-      <Toolbar />
-      <SimulationTimer />
-      <FloatingPlayPause />
-      <BottomLeftPill />
+      <div className="md:hidden">
+        <MobileBlocker />
+      </div>
+      <div className="hidden md:block">
+        <Header />
+        <Canvas />
+        <LeftSideBar />
+        <Toolbar />
+        <SimulationTimer />
+        <FloatingPlayPause />
+        <BottomLeftPill />
+      </div>
     </div>
   );
 }
