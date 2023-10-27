b050cb13ccd74cc909bb4d64c0c61d5c6a7316ad
Taz17 <mutaz.ashhab@gmail.com>
2023-10-27 16:06:04 +1100

    add JSDOC for urban file logic

--- src/logic/urbanflo-file-logic.ts
+++ src/logic/urbanflo-file-logic.ts
@@ -5,6 +5,9 @@ import {
   useSimulationHistory,
 } from '~/zustand/useSimulationHistory';
 
+/**
+ * Defines the structure of an UrbanFlo file.
+ */
 interface UrbanFloFile {
   version: number;
   networkData: NetworkData;
@@ -14,6 +17,11 @@ interface UrbanFloFile {
   };
 }
 
+/**
+ * Returns the content of an UrbanFlo file as a string.
+ *
+ * @returns {string} - String representation of the UrbanFlo file.
+ */
 export function getUrbanFloFileContents(): string {
   const fileContents = getFileContents();
 
@@ -22,6 +30,11 @@ export function getUrbanFloFileContents(): string {
   return jsonString;
 }
 
+/**
+ * Retrieves the contents required for an UrbanFlo file.
+ *
+ * @returns {UrbanFloFile} - The contents of the UrbanFlo file.
+ */
 function getFileContents(): UrbanFloFile {
   const networkStore = useNetworkStore.getState();
   const simulationHistoryStore = useSimulationHistory.getState();
@@ -45,6 +58,12 @@ function getFileContents(): UrbanFloFile {
   };
 }
 
+/**
+ * Converts a JSON object to an UrbanFloFile.
+ *
+ * @param {unknown} json - The JSON object to be converted.
+ * @returns {UrbanFloFile} - The UrbanFloFile representation of the provided JSON.
+ */
 export function getNetworkFromUploadedFile(json: unknown): UrbanFloFile {
   return json as UrbanFloFile;
 }
